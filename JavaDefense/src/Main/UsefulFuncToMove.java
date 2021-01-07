0724516b70277fed9669c38acd4d58deb0a10b33
Arcanis <djinduvent@hotmail.fr>
2021-01-07 15:05:22 +0100

    test func

--- JavaDefense/src/Main/UsefulFuncToMove.java
+++ JavaDefense/src/Main/UsefulFuncToMove.java
@@ -7,7 +7,8 @@ public final class UsefulFuncToMove {
 	
 	public static PositionTab mouseClickToGridPos(double x_curs, double y_curs) 
 	{
-		
+		System.out.println(x_curs*World.taille);
+		System.out.println(y_curs*World.taille);
 		return null;
 	}
 }
