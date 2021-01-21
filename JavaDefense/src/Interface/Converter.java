709a2606cb541eea6f764906e55e5844a6f15128
Enoal <enoal.gesny@gmail.com>
2021-01-21 12:54:22 +0100

    TODO partie2

--- JavaDefense/src/Interface/Converter.java
+++ JavaDefense/src/Interface/Converter.java
@@ -6,12 +6,16 @@ public final class Converter {
 	public static PositionTab positionToTab(Position p) 
 	{
 		// TODO Auto-generated method stub
-		return null;
+		int x = (int)p.getX();
+		int y = (int)p.getY();
+		return new PositionTab(x,y);
 	}
 	
 	public static Position tabToPosition(PositionTab pt) 
 	{
 		// TODO Auto-generated method stub
-		return null;
+		double x = (double)pt.getX();
+		double y = (double)pt.getY();
+		return new Position(x,y);
 	}
 }
