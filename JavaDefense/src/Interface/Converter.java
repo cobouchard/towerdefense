5945e24aeead39e8271c33de408cff90acda0bac
Enoal <enoal.gesny@gmail.com>
2021-01-21 21:48:31 +0100

    Converter v2

--- JavaDefense/src/Interface/Converter.java
+++ JavaDefense/src/Interface/Converter.java
@@ -1,21 +1,26 @@
 package Interface;
 
 import AStar.PositionTab;
+import Main.World;
 
 public final class Converter {
 	public static PositionTab positionToTab(Position p) 
 	{
 		// TODO Auto-generated method stub
-		int x = (int)p.getX();
-		int y = (int)p.getY();
+		double xp = p.getX();
+		double yp = p.getY();
+		int x = (int)(xp*750/15);
+		int y = (int)(yp*750/15);
 		return new PositionTab(x,y);
 	}
 	
 	public static Position tabToPosition(PositionTab pt) 
 	{
 		// TODO Auto-generated method stub
-		double x = (double)pt.getX();
-		double y = (double)pt.getY();
+		int xpt = pt.getX();
+		int ypt = pt.getY();
+		double x = xpt*15/750;
+		double y = ypt*15/750;
 		return new Position(x,y);
 	}
 }
